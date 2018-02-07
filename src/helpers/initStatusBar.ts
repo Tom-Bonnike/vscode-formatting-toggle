5c9dda65c6893fa531409b216dbefa5c6847ae61
Tom Bonnike <bonniketom@gmail.com>
2018-02-07 13:59:45 +0100

    Fix status bar priority swap with Prettier

--- src/helpers/initStatusBar.ts
+++ src/helpers/initStatusBar.ts
@@ -1,9 +1,15 @@
 import { window, StatusBarAlignment, StatusBarItem } from 'vscode'
 import { COMMAND_NAME, ENABLED_TEXT } from '../constants'
 
+const PRETTIER_STATUS_BAR_PRIORITY = -1
 const initStatusBar = (): StatusBarItem => {
-  // `-1` matches the Prettier extension’s status bar priority.
-  const statusBar = window.createStatusBarItem(StatusBarAlignment.Right, -1)
+  // `-1` matches the Prettier extension’s status bar priority. We substract
+  // one so that it is always to the right. If we don’t do this, the position
+  // sometimes switches randomly.
+  const statusBar = window.createStatusBarItem(
+    StatusBarAlignment.Right,
+    PRETTIER_STATUS_BAR_PRIORITY - 1
+  )
   statusBar.command = `extension.${COMMAND_NAME}`
   statusBar.tooltip = 'Enable/Disable formatting'
   statusBar.text = ENABLED_TEXT
