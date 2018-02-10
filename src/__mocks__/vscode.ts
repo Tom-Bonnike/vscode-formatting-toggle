10c7285892c70c92503dd8ecb073bd0a61e4e1ce
Tom Bonnike <bonniketom@gmail.com>
2018-02-10 15:11:13 +0100

    Add tests for initStatusBar

--- src/__mocks__/vscode.ts
+++ src/__mocks__/vscode.ts
@@ -1,3 +1,14 @@
 export const ConfigurationTarget = {
   Global: 1
 }
+
+export const StatusBarAlignment = {}
+
+export const window = {
+  createStatusBarItem: () => ({
+    command: '',
+    tooltip: '',
+    text: '',
+    show: jest.fn()
+  })
+}
